9e6155e6b1fea04faa79f44b93be0364ff1de594
Joao Mirante <joaomirante16@gmail.com>
2018-09-26 02:03:02 +0100

    v1

--- app/src/main/java/vilares/mirante/androidperguntas/Fragment/FragmentHome.java
+++ app/src/main/java/vilares/mirante/androidperguntas/Fragment/FragmentHome.java
@@ -2,10 +2,16 @@ package vilares.mirante.androidperguntas.Fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 import vilares.mirante.androidperguntas.R;
 
@@ -14,7 +20,8 @@ import vilares.mirante.androidperguntas.R;
  */
 public class FragmentHome extends Fragment {
 
-
+    RecyclerView rv;
+    View v;
     public FragmentHome() {
         // Required empty public constructor
     }
@@ -24,7 +31,57 @@ public class FragmentHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_home, container, false);
+        v = inflater.inflate(R.layout.fragment_fragment_home, container, false);
+
+        rv = v.findViewById(R.id.rv_jogos);
+
+        rv.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext(),LinearLayoutManager.HORIZONTAL,false));
+
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("jogo1");
+        strings.add("jogo2");
+        strings.add("jogo3");
+
+        rv.setAdapter(new AAJogo(strings));
+
+
+
+        return v;
+    }
+
+
+    public class AAJogo extends RecyclerView.Adapter<AAJogo.VHJogo>{
+
+        ArrayList<String> strings;
+
+        public AAJogo(ArrayList<String> strings) {
+            this.strings = strings;
+        }
+
+        @NonNull
+        @Override
+        public VHJogo onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(getActivity().getApplicationContext()).inflate(R.layout.cv_jogo,parent,false);
+            return new VHJogo(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull VHJogo holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return strings.size();
+        }
+
+        public class VHJogo extends RecyclerView.ViewHolder{
+
+
+            public VHJogo(View itemView) {
+                super(itemView);
+            }
+        }
     }
 
 }
--- app/src/main/java/vilares/mirante/androidperguntas/MainActivity.java
+++ app/src/main/java/vilares/mirante/androidperguntas/MainActivity.java
@@ -1,13 +1,79 @@
 package vilares.mirante.androidperguntas;
 
+import android.os.AsyncTask;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import net.eunainter.r2std2oid.RequestR2D2;
+import net.eunainter.r2std2oid.Skyrunner;
+
+import java.util.List;
+
+import vilares.mirante.androidperguntas.Fragment.FragmentHome;
+import vilares.mirante.androidperguntas.Service.RestConnect;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    FrameLayout frameLayout;
+
+    RestConnect restConnect;
+
+    FragmentHome fragmentHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        fragmentHome = new FragmentHome();
+
+        setFragment(fragmentHome);
+
+        restConnect = new RestConnect("", RequestR2D2.GET,null);
+
+
+
+    }
+
+    public class get extends AsyncTask<String,Integer,Integer> {
+        @Override
+        protected void onPreExecute(){
+            //Codigo
+
+            restConnect = new RestConnect("http://ip:porta/", RequestR2D2.POST,null);
+        }
+        @Override
+        protected Integer doInBackground(String... params) {
+
+            while (!restConnect.isFinish()){}
+            return 1;
+        }
+        @Override
+        protected void onPostExecute(Integer numero){
+            if(numero==1){
+                System.out.println(restConnect.getJsonArray().toString());
+                incendioArrayList = new Gson().fromJson(restConnect.getJsonArray().toString(),new TypeToken<List<Incendio>>(){}.getType());
+            }
+            carregarmap();
+
+            //Codigo
+        }
+
+
+    }
+
+
+    private void setFragment(Fragment fragment){
+
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
+
     }
 }
